2edb12e55b16b7f9b1e9b6d15541579a4953623d
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-06-09 19:25:18 +0300

    Terrnar++

--- Split3/main.cpp
+++ Split3/main.cpp
@@ -13,13 +13,13 @@ void main()
 	Print(arr, n);
 	int even_count = 0;
 	int odd_count = 0;
-	for (int i = 0; i < n; i++)arr[i] % 2 == 0 ? even_count++: odd_count++;
+	for (int i = 0; i < n; i++)(arr[i] % 2? odd_count : even_count )++;
 	cout << "even= " << even_count << endl;
 	cout << "odd= " << odd_count << endl;
 	int* even_arr = new int[even_count];
 	int* odd_arr = new int[odd_count];
 
-	for (int i = 0, j = 0, k = 0; i < n; i++) arr[i] % 2 == 0 ? even_arr[j++] = arr[i] : odd_arr[k++] = arr[i];
+	for (int i = 0, j = 0, k = 0; i < n; i++) (arr[i] % 2? odd_arr[k++]: even_arr[j++]) = arr[i];
 
 	Print(even_arr, even_count);
 	Print(odd_arr, odd_count);
