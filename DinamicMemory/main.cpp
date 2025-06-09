9b7fd992a444970261d7b1908d3c6c9480677b1b
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-06-09 21:25:36 +0300

    TODO: insert(), erase()

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+int* push_back(int arr[],int& n, int value);
+int* push_front(int arr[], int& n, int value);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
 
 void main()
 {
@@ -14,8 +19,25 @@ void main()
 
 	FillRand(arr, n);
 	Print(arr, n);
+
+	int value;
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+	
+
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr=push_front(arr, n, value);
+	Print(arr, n);
+	arr = pop_back(arr, n);
+	Print(arr, n);
+	
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
 	delete[] arr;
 }
+
 
 void FillRand(int arr[], const int n)
 {
@@ -26,11 +48,52 @@ void FillRand(int arr[], const int n)
 }
 void Print(int arr[], const int n)
 {
-	cout << arr << endl;
-	cout << *arr << endl;
+	//cout << arr << endl;
+	//cout << *arr << endl;
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";//subscript operator
 	}
 	cout << endl;
+}
+
+int* push_back(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	buffer = nullptr;//nullptr - указатель на ноль.
+	arr[n] = value;
+	n++;
+	return arr;
+}
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i+1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++) buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[]arr;
+	return buffer;
 }
